168a51752030aa3863fd610aaf30684b411b1e11
satvik1311 <satvik.gaur12@gmail.com>
2026-01-31 16:46:53 +0530

    Add initial datatype.js file with variable declarations and type comments

---
+++ datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello World!"); // we are using node js not browser islye ni chlra 
+
+// maintain code reability and clean code
+
+let name = "Satvik";
+let age =  21;
+let isLoggedIn = false;
+
+// number => 2 to power 53 -1
+// BigInt => larger than 2 to power 53 -1
+// string => "";
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique value (uniqueness ke liye kaam ata h)
+
+
+// object => key value pairs 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (bug)
+
+
